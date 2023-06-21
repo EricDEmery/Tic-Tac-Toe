90e6dd7ff1faeb11ee9bf952962c26c889afdaf1
Eric Emery <ericemery099@gmail.com>
2023-06-21 18:50:15 -0400

    updated JS

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,15 @@
 const app = document.getElementById("app")
+const reset = document.createElement("button")
+        reset.innerText = "Reset"
+const start = document.createElement("button")
+        start.innerText = "Start Game"
 
 let gameState = {
 
     gameActive: "true",
     playerTurn: "X",
     turnCounter: 0,
-    winner:""
+    winner:"",
     gameBoard: [
         [0, 1, 2],
         [3, 4, 5],
