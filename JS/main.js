632e23555fb93ddfb4a34beca4c90507d896342e
Eric Emery <ericemery099@gmail.com>
2023-06-21 18:37:50 -0400

    updated js

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+const app = document.getElementById("app")
+
+let gameState = {
+
+    gameActive: "true",
+    playerTurn: "X",
+    turnCounter: 0,
+    winner:""
+    gameBoard: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]    
+    ]
+}
